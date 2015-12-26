58e19fc34c7637739139ba4ad5a297ba3fc0c7f3
Ryan Davis <3.1415927ryan@gmail.com>
2015-12-26 00:52:08 -0500

    Added some higher order functions

--- LearnYouAHaskell/6_HigherOrderFunctions.hs
+++ LearnYouAHaskell/6_HigherOrderFunctions.hs
@@ -12,3 +12,20 @@ multThree x y z = x * y * z
 divideByTen :: (Floating a) => a -> a
 divideByTen = (/10)
 
+-- functions using functions are parameters
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+-- zipWith applies some function to two lists zipping them respectively
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+-- flip takes in a function and flips their parameters
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+    where g x y = f y x 
+    
+flip2 :: (a -> b -> c) -> b -> a -> c
+flip2 f y x = f x y
