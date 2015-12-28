92d402542f1169fd2b39856569d136f1f07ef565
Ryan Davis <3.1415927ryan@gmail.com>
2015-12-28 14:35:26 -0500

    Onto modules

--- LearnYouAHaskell/6_HigherOrderFunctions.hs
+++ LearnYouAHaskell/6_HigherOrderFunctions.hs
@@ -64,6 +64,56 @@ chain n
     | even n = n : chain (n `div` 2)
     | odd n  = n : chain (n * 3 + 1)
     
+    -- maximum [(x, maximum $ chain x) | x <- [1..1000]]
+highChain :: (Integral a) => a -> a -> (a, a)
+highChain n m = maximum [(maximum $ chain x, x) | x <- [n..m]]
+    
 -- Lambdas sole purpose is to be passed on to higher order functions
 numLongChains :: Int
 numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+-- flip using lambdas expressions
+flip1 :: (a -> b -> c) -> b -> a -> c
+flip1 f = \x y -> f y x 
+-- flip1 with parameter f equals lambda expression
+-- with parameters x and y that computes f y x
+
+-- folding, accumulating a list
+sum' :: (Num a) => [a] -> a
+sum' xs = fold' (\acc x -> acc + x) 0 xs
+-- equal to
+sum2 :: (Num a) => [a] -> a
+sum2 = foldl (+) 0
+-- we can omit xs because of currying
+-- it will return a function that takes a list
+-- generally if you have a function like
+------ foo a = bar b a
+-- you can rewrite it as
+------ foo = bar b 
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+-- right fold!
+map2 :: (a -> b) -> [a] -> [b]
+map2 f xs = foldr (\x acc -> f x : acc) [] xs
+
+-- attempt to implement fold
+fold' :: (a -> a -> a) -> a -> [a] -> a
+fold' _ acc [] = acc
+fold' f acc (x:xs) = f (fold' f acc xs) x  
+
+-- reverse in folds
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
+
+(++++) :: (Num a) => a -> a -> a
+x ++++ y = x + y + x + y
+
+-- function composition
+-- (F o G)(x) = F(G(x))
+-- using the '.' function
+-- (.) :: (b -> c) -> (a -> b) -> a -> c
+-- f . g = \x -> f (g x)
+
+fn = ceiling . negate . tan . cos . max 50 
---
+++ LearnYouAHaskell/7_Modules.hs
@@ -0,0 +1,57 @@
+-- import in Haskell File
+import Data.List
+
+-- import in global namespace in ghci
+--  --  :m + Data.List
+
+-- import several modules in global
+--  --  :m + Data.List Data.Map Data.Set
+
+-- import just a couple functions from a module
+--  --  import Data.List (nub, sort)
+
+-- import all except a few
+--  --  import Data.List hiding (nub)
+
+-- import modules with same name functions
+--  --  import qualified Data.Map
+-- reference by 
+--  --  Data.Map.filter
+
+-- or import like this:
+--  --  import qualified Data.Map as M
+-- reference by
+--  --  M.filter
+
+-- Standard library : 
+-- http://www.haskell.org/ghc/docs/latest/html/libraries/
+-- locate functions
+-- http://haskell.org/hoogle
+
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . nub
+
+-- using a fold for searching a list for a sublist
+search :: (Eq a) => [a] -> [a] -> Bool
+search needle haystack =
+    let nlen = length needle
+    in  foldl (\acc x -> if take nlen x == needle then True else acc) False $ tails haystack
+    
+-- finding the largest proper prefix
+preSuffix :: (Eq a) => [a] -> [a]
+preSuffix [] = []
+preSuffix xs = foldl' func [] $ (init . inits) xs
+    where func = (\acc x -> if x `isSuffixOf` xs then x else acc)
+    
+-- implementation of the kmp algorithm
+-- failure function
+failure :: String -> [Int]
+failure [] = error "Must not be null!"
+failure xs = table
+    where table = -1:[length (preSuffix x) | x <- tail (inits xs)]
+
+func :: String -> Int -> Int -> Int
+func pos cnd 
+    | pos == 0  = -1
+    | pos == 1  = 0
+    | otherwise = 
